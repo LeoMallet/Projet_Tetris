ce88760738d21ad7fe47f8b89efae59dabd37cab
Unknown <mariealizee.mechain@gmail.com>
2018-01-19 15:10:53 +0100

    Auto stash before merge of "master" and "origin/master"

--- tetris_dal/src/main/java/dao/IAuthentificationDAO.java
+++ tetris_dal/src/main/java/dao/IAuthentificationDAO.java
@@ -1,9 +1,13 @@
 package dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import metier.Player;
 
+<<<<<<< Updated upstream
+import metier.Player;
+
+=======
+>>>>>>> Stashed changes
 public interface IAuthentificationDAO extends JpaRepository <Player, Integer> {
 
 }
