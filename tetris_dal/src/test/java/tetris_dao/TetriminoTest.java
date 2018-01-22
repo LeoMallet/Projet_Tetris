11d869cc61fdbeac6663dd89f4683e7d30d8e889
LéoMallet <leomalletdida@gmail.com>
2018-01-22 10:19:11 +0100

    creation de TetriminoTest

---
+++ tetris_dal/src/test/java/tetris_dao/TetriminoTest.java
@@ -0,0 +1,14 @@
+package tetris_dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TetriminoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
