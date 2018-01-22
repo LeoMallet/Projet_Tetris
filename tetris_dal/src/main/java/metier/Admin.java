634f09f935e4440b1c2a2ae97395799875fa8300
Unknown <mariealizee.mechain@gmail.com>
2018-01-22 15:33:13 +0100

    Admin terminé

--- tetris_dal/src/main/java/metier/Admin.java
+++ tetris_dal/src/main/java/metier/Admin.java
@@ -1,6 +1,10 @@
 package metier;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -8,6 +12,52 @@ import javax.persistence.Table;
 @Table(name="admin")
 public class Admin extends User {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="ADM_ID")
+	private int id;
+	
+	@Column(name="ADM_LOGIN")
+	private String login;
+	
+	@Column(name="ADM_MDP")
+	private String mdp;
+	
 	@OneToOne(mappedBy="admin")
 	private Faq faq;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getMdp() {
+		return mdp;
+	}
+
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+
+	public Faq getFaq() {
+		return faq;
+	}
+
+	public void setFaq(Faq faq) {
+		this.faq = faq;
+	}
+	
+	
+	
 }
