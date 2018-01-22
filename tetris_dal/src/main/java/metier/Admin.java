25cad26208fd85d33024524e30cfa80a7d6dbaf4
Unknown <mariealizee.mechain@gmail.com>
2018-01-22 15:29:32 +0100

    ajout admin

--- tetris_dal/src/main/java/metier/Admin.java
+++ tetris_dal/src/main/java/metier/Admin.java
@@ -1,9 +1,13 @@
 package metier;
 
+import javax.persistence.Entity;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="admin")
 public class Admin extends User {
 
-	@OneToOne(mappedBy="faq")
+	@OneToOne(mappedBy="admin")
 	private Faq faq;
 }
